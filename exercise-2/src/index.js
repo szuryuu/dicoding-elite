d5cd2e36c92571a21a19211ba87e361e3a59678a
shfwnz <ilhamdzaky2007@gmail.com>
2025-07-27 13:08:33 +0700

    fix: code

--- exercise-2/src/index.js
+++ exercise-2/src/index.js
@@ -1,7 +1,13 @@
+// Code Review
+//
+// Terdapat Double render data notes, jika sudah menggunakan API lebih baik hapus fake data yang dipakai
+
 // Importing required modules and styles
 import "./styles/main.css"; // Import CSS (Webpack will bundle this)
 import "./components/NoteItem"; // Custom element/component
-import { fetchNotes, renderNotes } from "./components/notes";
+
+// Lebih baik gunakan data dari API saja
+// import { fetchNotes, renderNotes } from "./components/notes";
 import { getNotes, addNote, deleteNote, archiveNote } from "./api";
 import { showNotes } from "./components/NoteItem";
 
@@ -11,6 +17,7 @@ const addNoteBtn = document.getElementById("add-note-btn");
 const addNoteModal = document.getElementById("add-note-modal");
 const closeModalBtn = document.getElementById("close-modal");
 const noteForm = document.getElementById("note-form");
+const modalForm = document.getElementById("modal-note-form");
 
 // Pindahaan dari index.html
 const loadingIndicator = document.getElementById("loadingIndicator");
@@ -22,15 +29,18 @@ setTimeout(() => {
   loadingIndicator.style.display = "none";
 
   // Simulasi penampilan konten setelah loading selesai
-  document.getElementById("notes-grid").innerHTML =
-    "<p>Data berhasil dimuat!</p>";
+  // document.getElementById("notes-grid").innerHTML =
+  //   "<p>Data berhasil dimuat!</p>";
 }, 2000); // Simulasi delay
+// Pindahan selesai
 
+// Display API data
 // Display notes on the main page
 const displayNotes = async () => {
   try {
     const notes = await getNotes(); // Menggunakan getNotes
-    showNotes(notes, noteListContainer); // Render notes
+    const notesGrid = document.getElementById("notes-grid");
+    showNotes(notes, notesGrid); // Render notes
   } catch (error) {
     console.error("Error fetching notes:", error);
     alert("Failed to load notes");
@@ -99,12 +109,14 @@ const handleArchiveNote = async (noteId) => {
 addNoteBtn.addEventListener("click", openAddNoteModal);
 closeModalBtn.addEventListener("click", closeAddNoteModal);
 noteForm.addEventListener("submit", handleAddNote);
+modalForm.addEventListener("submit", handleAddNote);
 
 // Initialize the app with notes
 displayNotes();
 
+// Hapus data fake note
 // Fetch notes and render them when the page loads
-window.addEventListener("DOMContentLoaded", async () => {
-  const notes = await fetchNotes();
-  renderNotes(notes); // Render notes on page load
-});
+// window.addEventListener("DOMContentLoaded", async () => {
+//   const notes = await fetchNotes();
+//   renderNotes(notes); // Render notes on page load
+// });
